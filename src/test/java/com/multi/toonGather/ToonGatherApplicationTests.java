28aab3832a2fc3e4c5f315d421d1fca64e746a05
Kang Hyungoo <gooiinn@outlook.kr>
2024-07-22 14:36:06 +0900

    240722_initialization

---
+++ src/test/java/com/multi/toonGather/ToonGatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.toonGather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToonGatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
