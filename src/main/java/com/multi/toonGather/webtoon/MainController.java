3319e15924abf0d88d8473731c923666c375cbaf
박재형 <168509100+ParkJaeHyung00@users.noreply.github.com>
2024-07-23 09:06:16 +0900

    박재형 변경

---
+++ src/main/java/com/multi/toonGather/webtoon/MainController.java
@@ -0,0 +1,20 @@
+package com.multi.toonGather.webtoon;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+    @GetMapping(value = {"/","/main"})
+    public String main(){
+        return "main/main";
+    }
+
+
+    @PostMapping("/")
+    public String redirectMain(){
+        return "redirect:/";
+    }
+
+}
