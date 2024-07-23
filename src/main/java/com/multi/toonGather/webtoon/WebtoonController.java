623bbc79d3eb16474977b551fea627b5efd8f0bf
박재형 <168509100+ParkJaeHyung00@users.noreply.github.com>
2024-07-23 17:59:20 +0900

    WT01_웹툰 리스트 출력,상세 페이지 구현

--- src/main/java/com/multi/toonGather/webtoon/WebtoonController.java
+++ src/main/java/com/multi/toonGather/webtoon/WebtoonController.java
@@ -1,13 +1,22 @@
 package com.multi.toonGather.webtoon;
 
+import com.multi.toonGather.webtoon.model.WebtoonDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebtoonController {
     @GetMapping(value = {"/","webtoon/test"})
-    public String webtoon(){
+    public String Webtoon(){
         return "webtoon/webtoonlist";
+    }
+
+    @GetMapping(value = {"webtoon/one"})
+    public String Web_toon_One(WebtoonDTO webtoonDTO, Model model){
+        System.out.println(webtoonDTO);
+        model.addAttribute("one",webtoonDTO);
+        return "webtoon/one";
     }
 
 
