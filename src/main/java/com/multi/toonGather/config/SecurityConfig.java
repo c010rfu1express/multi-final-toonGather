897e00457dfc00767349814e1b7a0059d3abd024
swellk <168509043+swellk@users.noreply.github.com>
2024-07-23 22:09:37 +0900

    [박서영-240723-01] 창작자 등록, 구인/구직 CRUD, 지원하기 기능 구현

---
+++ src/main/java/com/multi/toonGather/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.multi.toonGather.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+
+        return (web) -> web.ignoring().requestMatchers(
+                "/css/**", "/js/**", "/img/**", "/uploadFiles/**"
+        );
+
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/clearMessage", "/recruit/**", "/uploadFiles/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        http
+                .formLogin((auth) -> auth.loginPage("/login")
+                        .loginProcessingUrl("/login").permitAll());
+
+        http
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .deleteCookies("JSESSIONID")
+                        .invalidateHttpSession(true)
+                        .logoutSuccessUrl("/"));
+
+        http
+                .sessionManagement((auth) -> auth
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true));
+
+        http
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .accessDeniedPage("/error/denied"));
+
+        http
+                .csrf((auth) -> auth.disable());
+
+
+        return http.build();
+    }
+}
+//"/admin" ,
---
+++ src/main/java/com/multi/toonGather/recruit/model/dto/creator/CreatorDTO.java
@@ -0,0 +1,13 @@
+package com.multi.toonGather.recruit.model.dto.creator;
+
+import lombok.Data;
+
+@Data
+public class CreatorDTO {
+    private int creator_no;
+    private int member_no;
+    private String content;
+    private String img;
+    private String status;
+
+}
