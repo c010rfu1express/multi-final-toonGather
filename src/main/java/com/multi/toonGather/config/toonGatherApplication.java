f8e54e6c72af5d010e69e11547ef162b0ebbd822
Kim HeeCheol <audwkd0618@gmail.com>
2024-07-24 09:32:11 +0900

    [김희철-240724-02] toonGatherApplication 수정
    - 공통파일 toonGatherApplication에 MapperScan을 삭제했습니다.

--- src/main/java/com/multi/toonGather/config/toonGatherApplication.java
+++ src/main/java/com/multi/toonGather/config/toonGatherApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.multi.toonGather.cs.model.mapper")
 public class toonGatherApplication {
 
 	public static void main(String[] args) {
