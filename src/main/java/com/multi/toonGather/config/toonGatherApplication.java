c2062f8abc22df2de144174f4137476552f60928
HGKANG <gooiinn@outlook.kr>
2024-07-24 09:19:21 +0900

    Revert "[최서윤-240723] feat :: 리뷰 작성 기능 구현"

--- src/main/java/com/multi/toonGather/config/toonGatherApplication.java
+++ src/main/java/com/multi/toonGather/config/toonGatherApplication.java
@@ -1,11 +1,9 @@
 package com.multi.toonGather.config;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.multi.toonGather.social.model.dao")
 public class toonGatherApplication {
 
 	public static void main(String[] args) {
