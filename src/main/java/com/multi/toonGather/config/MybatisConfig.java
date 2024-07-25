9a7b52b4e5b03923a104b159120743bb8153e150
SeoYunChoi <charlotteleechoi@gmail.com>
2024-07-25 10:50:20 +0900

    [최서윤-240725-01] fix :: MybatisConfig 수정

--- src/main/java/com/multi/toonGather/config/MybatisConfig.java
+++ src/main/java/com/multi/toonGather/config/MybatisConfig.java
@@ -38,14 +38,17 @@ public class MybatisConfig {
         configuration.setJdbcTypeForNull(JdbcType.NULL);
 
         // Type Aliases 설정
+        // 서영 recruit
         configuration.getTypeAliasRegistry().registerAlias("jobDTO", com.multi.toonGather.recruit.model.dto.job.JobDTO.class);
         configuration.getTypeAliasRegistry().registerAlias("pageDTO", com.multi.toonGather.common.model.dto.PageDTO.class);
         configuration.getTypeAliasRegistry().registerAlias("creatorDTO", CreatorDTO.class);
         configuration.getTypeAliasRegistry().registerAlias("applyDTO", com.multi.toonGather.recruit.model.dto.job.ApplyDTO.class);
         configuration.getTypeAliasRegistry().registerAlias("freeDTO", com.multi.toonGather.recruit.model.dto.free.FreeDTO.class);
+        // 희철 cs
         configuration.getTypeAliasRegistry().registerAlias("QuestionDTO", com.multi.toonGather.cs.model.dto.QuestionDTO.class);
         configuration.getTypeAliasRegistry().registerAlias("CsCategoryDTO", com.multi.toonGather.cs.model.dto.CsCategoryDTO.class);
         configuration.getTypeAliasRegistry().registerAlias("CsStatusDTO", com.multi.toonGather.cs.model.dto.CsStatusDTO.class);
+        // 서윤 social
         configuration.getTypeAliasRegistry().registerAlias("reviewDTO", com.multi.toonGather.social.model.dto.ReviewDTO.class);
         configuration.getTypeAliasRegistry().registerAlias("writerDTO", com.multi.toonGather.social.model.dto.WriterDTO.class);
         configuration.getTypeAliasRegistry().registerAlias("diaryDTO", com.multi.toonGather.social.model.dto.DiaryDTO.class);
