2785d78c5eb419d018ce4d6e31a0baf21ad63b77
Kang Hyungoo <gooiinn@outlook.kr>
2024-07-30 09:13:35 +0900

    [강현구-240730-01] feat :: 로그인 인증 구현/관리자 회원관리 구현 -로그인 인증 구현했습니다. -관리자페이지 회원관리(리스트, 상세) 구현했습니다.

--- src/main/java/com/multi/toonGather/user/service/UserService.java
+++ src/main/java/com/multi/toonGather/user/service/UserService.java
@@ -2,6 +2,8 @@ package com.multi.toonGather.user.service;
 
 import com.multi.toonGather.user.model.dto.UserDTO;
 
+import java.util.List;
+
 public interface UserService {
 
     void insertUser(UserDTO userDTO) throws Exception;
@@ -9,4 +11,6 @@ public interface UserService {
     String findPw(UserDTO userDTO) throws Exception;
     UserDTO getProfile(int userNo) throws Exception;
     void updateProfile(int userNo, UserDTO userDTO) throws Exception;
+    void deleteProfile(int userNo) throws Exception;
+    List<UserDTO> getUsers() throws Exception;
 }
