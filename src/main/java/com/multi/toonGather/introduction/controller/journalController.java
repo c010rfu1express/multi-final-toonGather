0a1490462388c4c33f98055447cb62f0702be1b3
KimDongHyeon <hyun70022@gmail.com>
2024-07-31 09:02:50 +0900

    [김동현-240731-01] fix :: 소식게시판 첨부파일 저장 및 출력 수정

--- src/main/java/com/multi/toonGather/introduction/controller/journalController.java
+++ src/main/java/com/multi/toonGather/introduction/controller/journalController.java
@@ -1,8 +1,7 @@
 package com.multi.toonGather.introduction.controller;
 
 import com.multi.toonGather.introduction.model.dto.JournalDTO;
-import com.multi.toonGather.introduction.model.dto.JournalFileDTO;
-import com.multi.toonGather.introduction.service.JournalService;
+import com.multi.toonGather.introduction.service.JournalServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,12 +16,13 @@ import java.util.List;
 public class journalController {
 
     @Autowired
-    private JournalService journalService;
+    private JournalServiceImpl journalService;
 
 
     @GetMapping(value = {"introduction/journalList"})
     public String journalList(Model model){
-        List<JournalDTO> journals = journalService.getAllJournals();
+//        List<JournalDTO> journals = journalService.getAllJournals();
+        List<JournalDTO> journals = journalService.getAllJournalsWithFiles();
         System.out.println("Retrieved journals: " + journals);  // 로그 출력 : 실제로 데이터가 반환되고 있는지 확인하기 위해, 컨트롤러에서 로그를 출력
         model.addAttribute("journals", journals);
         return "introduction/journalList";
@@ -49,7 +49,11 @@ public class journalController {
 
     @GetMapping(value = {"introduction/journalDetail"})
     public String journalDetail(@RequestParam(value = "title", required = true) String title, Model model){
-        JournalDTO journalDTO = journalService.getJournalByTitle(title);
+        JournalDTO journalDTO = journalService.getJournalByTitleWithFile(title);
+
+
+
+        System.out.println("Retrieved journal: " + journalDTO);
         model.addAttribute("journal", journalDTO);
         return "introduction/journalDetail";
 
