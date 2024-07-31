d3bba99ba000e1c0a1356518182dd734d754776b
SeoYunChoi <charlotteleechoi@gmail.com>
2024-07-31 15:18:33 +0900

    [최서윤-240731-03] build :: 에러 처리 파일, 에러 페이지 생성 1. 에러 처리 클래스 파일 생성   - com.multi.toonGather.common.exception 2. 에러 html 생성   - resources/templates/common/error/
    **Whitelabel 페이지 보며 작업하고 싶은 분들은 지우고 작업하시면 됩니다. (지운 걸 커밋/푸시하진 마세요.)
    
    **모든 예외는 GlobalExceptionHandler에서 처리
    **NotFoundException은 404 에러 페이지로 연결
    **AccessDeniedException은 403 에러 페이지로 연결
    **그 외의 예외는 일반 에러 페이지(general)로 연결
    **각 메서드에서는 적절한 상황에 맞는 예외를 throw
    **@Controller/@Service 단계에서 에러 메세지와 함께 Exception/NotFoundException/AccessDeniedException throw하기

---
+++ src/main/java/com/multi/toonGather/common/exception/NotFoundException.java
@@ -0,0 +1,14 @@
+package com.multi.toonGather.common.exception;
+
+/**
+ * Please explain the class!!
+ *
+ * @author : seoyun
+ * @fileName : NotFoundException
+ * @since : 2024-07-31
+ */
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
