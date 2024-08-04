436e509006415d15269a0987899d4e25af897eae
KimDongHyeon <hyun70022@gmail.com>
2024-08-04 14:37:22 +0900

    [김동현-240804-01] feat :: 소식게시판 관리자, 회원 페이지 구분

--- src/main/java/com/multi/toonGather/introduction/controller/JournalController.java
+++ src/main/java/com/multi/toonGather/introduction/controller/JournalController.java
@@ -16,41 +16,63 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class journalController {
+public class JournalController {
 
     @Autowired
     private JournalServiceImpl journalService;
 
+    @GetMapping(value = {"introduction/main"})
+    public String inMain() {
+        return "introduction/main";
+    }
 
-    @GetMapping(value = {"introduction/journalList"})
-    public String journalList(Model model){
-//        List<JournalDTO> journals = journalService.getAllJournals();
+    @GetMapping(value = {"introduction/journal/admin/journalListAdmin"})
+    public String journalListAdmin(Model model){
+
         List<JournalDTO> journals = journalService.getAllJournalsWithFiles();
+
         System.out.println("Retrieved journals: " + journals);  // 로그 출력 : 실제로 데이터가 반환되고 있는지 확인하기 위해, 컨트롤러에서 로그를 출력
+
         model.addAttribute("journals", journals);
-        return "introduction/journalList";
+
+        return "introduction/journal/admin/journalListAdmin";
     }
 
-    @GetMapping(value = {"introduction/journalInsert"})
+    @GetMapping(value = {"introduction/journal/user/journalListUser"})
+    public String journalListUser(Model model){
+
+        List<JournalDTO> journals = journalService.getAllJournalsWithFiles();
+
+        System.out.println("Retrieved journals: " + journals);  // 로그 출력 : 실제로 데이터가 반환되고 있는지 확인하기 위해, 컨트롤러에서 로그를 출력
+
+        model.addAttribute("journals", journals);
+
+        return "introduction/journal/user/journalListUser";
+    }
+
+    @GetMapping(value = {"introduction/journal/admin/journalInsert"})
     public String journalInsertForm(){
-        return "introduction/journalInsert";
+        return "introduction/journal/admin/journalInsert";
     }
 
-    @PostMapping(value = {"introduction/journalInsert"})
+    @PostMapping(value = {"introduction/journal/admin/journalInsert"})
     public String journalInsert(@RequestParam("title") String title,
                                 @RequestParam("content") String content,
                                 @RequestParam("file") MultipartFile file,
                                 HttpServletRequest request) {
         try {
             journalService.insertJournal(title, content, file, request);
-            return "redirect:/introduction/journalList";
+            System.out.println("이거확인111");
+            return "redirect:/introduction/journal/admin/journalListAdmin";
+//            return "introduction/journal/admin/journalListAdmin";
+//            return "introduction/journal/user/journalListUser";
         } catch (Exception e) {
             // 에러 처리
             return "error";
         }
     }
 
-    @GetMapping(value = {"introduction/journalUpdate"})
+    @GetMapping(value = {"introduction/journal/admin/journalUpdate"})
     public String journalUpdateForm(@RequestParam(value = "title", required = true) String title, Model model){
 
         JournalDTO journalDTO = journalService.getJournalByTitleWithFile(title);
@@ -58,10 +80,10 @@ public class journalController {
         System.out.println("Retrieved journal 수정페이지 : " + journalDTO);
         model.addAttribute("journal", journalDTO);
 
-        return "introduction/journalUpdate";
+        return "introduction/journal/admin/journalUpdate";
     }
 
-    @PostMapping(value = {"introduction/journalUpdate"})
+    @PostMapping(value = {"introduction/journal/admin/journalUpdate"})
     public String journalUpdate(@RequestParam("journalNo") int journalNo,
                                 @RequestParam("title") String title,
                                 @RequestParam("content") String content,
@@ -73,9 +95,11 @@ public class journalController {
         journal.setTitle(title);
         journal.setContent(content);
 
+        System.out.println("확인 44");
         boolean isSuccess = journalService.updateJournal(journal, file, request);
-        if(isSuccess) return "redirect:/introduction/journalList";
-        else return "redirect:/introduction/journalUpdate";
+        System.out.println("확인 55");
+        if(isSuccess) return "redirect:/introduction/journal/admin/journalListAdmin";
+        else return "introduction/journal/admin/journalUpdate";
     }
 
 
@@ -94,14 +118,25 @@ public class journalController {
         return "error";
     }
 
-    @GetMapping(value = {"introduction/journalDetail"})
-    public String journalDetail(@RequestParam(value = "title", required = true) String title, Model model){
+    @GetMapping(value = {"introduction/journal/admin/journalDetailAdmin"})
+    public String journalDetailAdmin(@RequestParam(value = "title", required = true) String title, Model model){
 
         JournalDTO journalDTO = journalService.getJournalByTitleWithFile(title);
 
         System.out.println("Retrieved journal 상세페이지 : " + journalDTO);
         model.addAttribute("journal", journalDTO);
 
-        return "introduction/journalDetail";
+        return "introduction/journal/admin/journalDetailAdmin";
+    }
+
+    @GetMapping(value = {"introduction/journal/user/journalDetailUser"})
+    public String journalDetailUser(@RequestParam(value = "title", required = true) String title, Model model){
+
+        JournalDTO journalDTO = journalService.getJournalByTitleWithFile(title);
+
+        System.out.println("Retrieved journal 상세페이지 : " + journalDTO);
+        model.addAttribute("journal", journalDTO);
+
+        return "introduction/journal/user/journalDetailUser";
     }
 }
