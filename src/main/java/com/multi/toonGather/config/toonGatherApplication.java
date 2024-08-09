4cda6019f57dc9bc422f41705ddccee9ba893d86
Kim HeeCheol <audwkd0618@gmail.com>
2024-08-09 09:31:45 +0900

    [김희철-240809-1] feat :: 챗봇 연동 개발 중 임시 저장
    - 챗봇 연동 개발 중 임시 저장

--- src/main/java/com/multi/toonGather/config/toonGatherApplication.java
+++ src/main/java/com/multi/toonGather/config/toonGatherApplication.java
@@ -2,8 +2,10 @@ package com.multi.toonGather.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.multi.toonGather.cs.client")
 public class toonGatherApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/multi/toonGather/cs/model/dto/Bubble.java
@@ -0,0 +1,9 @@
+package com.multi.toonGather.cs.model.dto;
+
+import lombok.Data;
+
+@Data
+public class Bubble {
+    private String type;
+    private TextData data;
+}
