9066a8db72fbcfb6b678d86def8c39c5846f84bb
Kang Hyungoo <gooiinn@outlook.kr>
2024-08-17 21:23:14 +0900

    [강현구-240817-14] chore :: 나의웹툰_북마크한웹툰 -- tags 중에 최초의 해시태그만 출력되도록 적용했습니다.

--- src/main/java/com/multi/toonGather/user/model/dto/MyWtWebtoonDTO.java
+++ src/main/java/com/multi/toonGather/user/model/dto/MyWtWebtoonDTO.java
@@ -22,4 +22,23 @@ public class MyWtWebtoonDTO {
     private String genre;
     private String tags;
     private int count;
+
+    public String getFirstHashtag() {
+        if (tags == null || tags.isEmpty()) {
+            return "";
+        }
+
+        // Find the start and end of the first hashtag
+        int startIndex = tags.indexOf('#'); // The index of the first #
+        if (startIndex == -1) {
+            return "";
+        }
+
+        int endIndex = tags.indexOf('#', startIndex + 1); // The index of the second #
+        if (endIndex == -1) {
+            endIndex = tags.length(); // No second #, so take the rest of the string
+        }
+
+        return tags.substring(startIndex, endIndex);
+    }
 }
