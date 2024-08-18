27e5d557b4caabd047a923579df63a0e1b256285
Kang Hyungoo <gooiinn@outlook.kr>
2024-08-19 04:01:28 +0900

    [강현구-240818-17] fix :: ID찾기 -- 로직에 결함이 있어 논리적으로 수정했습니다.

--- src/main/java/com/multi/toonGather/user/service/UserServiceImpl.java
+++ src/main/java/com/multi/toonGather/user/service/UserServiceImpl.java
@@ -89,34 +89,52 @@ public class UserServiceImpl implements UserService {
         UserDTO response2 = userMapper.selectOneByNickname(userDTO.getNickname());
 
         System.out.println("userDTO: "+ userDTO);   //request
-        System.out.println("response: "+ response); //response1 : 이메일을 통해 검색
-        System.out.println("response2: "+ response2); //response2 : 닉네임을 통해 검색
-        try{
-            if(userDTO.getNickname().equals(response.getNickname())) return response.getUserId();
-            else if(response2.getTypeCode() == 'K') {//소셜 가입후 이메일을 수정한경우
-                System.out.println("[UserService.findId] 카카오");
-                return "RETURNMESSAGE:ALREADYSIGNEDUPBYKAKAOAPI";
+        System.out.println("response: "+ response); //response1 : 이메일을 통해 조회해온 결과
+        System.out.println("response2: "+ response2); //response2 : 닉네임을 통해 조회해온 결과
+        if(response == null || response2 == null) {
+            if(response == null && response2 != null) { //닉네임은 찾았으나 이메일 못찾음
+                if(response2.getTypeCode() == 'K') {
+                    System.out.println("[UserService.findId] 카카오");
+                    return "RETURNMESSAGE:ALREADYSIGNEDUPBYKAKAOAPI";
+                }
+                else if(response2.getTypeCode() == 'N') {
+                    System.out.println("[UserService.findId] 네이버");
+                    return "RETURNMESSAGE:ALREADYSIGNEDUPBYNAVERAPI";
+                }
             }
-            else if(response2.getTypeCode() == 'N') {
-                System.out.println("[UserService.findId] 네이버");
-                return "RETURNMESSAGE:ALREADYSIGNEDUPBYNAVERAPI";
-            }
-            else {
-                System.out.println("[MESSAGE] 불일치. nickname: "+userDTO.getNickname()+" email: "+userDTO.getEmail());
-                return null;
-            }
-        } catch(Exception e){ //response가 null일 때. 이메일로 검색했더니 존재하지 않을 때
-            if(response2.getTypeCode() == 'K') {
-                System.out.println("[UserService.findId] 카카오");
-                return "RETURNMESSAGE:ALREADYSIGNEDUPBYKAKAOAPI";
-            }
-            else if(response2.getTypeCode() == 'N') {
-                System.out.println("[UserService.findId] 네이버");
-                return "RETURNMESSAGE:ALREADYSIGNEDUPBYNAVERAPI";
-            }
-
-            System.err.println("[UserService.findId] getProfile 예외처리 : response or response2가 null입니다! " + e.getMessage());
             return null;
+        }
+        else { //둘다 not null일 때는, response1(이메일)과 response2(닉네임)의 각각 userNo가 일치하는지부터 본다.
+            if(response.getUserNo() == response2.getUserNo()){
+                if(response.getTypeCode() == 'K') {
+                    System.out.println("[UserService.findId] 카카오");
+                    return "RETURNMESSAGE:ALREADYSIGNEDUPBYKAKAOAPI";
+                }
+                else if(response.getTypeCode() == 'N') {
+                    System.out.println("[UserService.findId] 네이버");
+                    return "RETURNMESSAGE:ALREADYSIGNEDUPBYNAVERAPI";
+                }
+                else return response.getUserId();
+            }
+            else return null;
+//            try{
+//                if(userDTO.getNickname().equals(response.getNickname())) return response.getUserId();
+//                else if(response2.getTypeCode() == 'K') {//소셜 가입후 이메일을 수정한경우
+//                    System.out.println("[UserService.findId] 카카오");
+//                    return "RETURNMESSAGE:ALREADYSIGNEDUPBYKAKAOAPI";
+//                }
+//                else if(response2.getTypeCode() == 'N') {
+//                    System.out.println("[UserService.findId] 네이버");
+//                    return "RETURNMESSAGE:ALREADYSIGNEDUPBYNAVERAPI";
+//                }
+//                else {
+//                    System.out.println("[MESSAGE] 불일치. nickname: "+userDTO.getNickname()+" email: "+userDTO.getEmail());
+//                    return null;
+//                }
+//            } catch(Exception e){
+//        }
+//
+//            return null;
         }
 //        if(userDTO.getNickname() == response.getNickname()) return response.getUserId();
 //        else return "ERROR";    //추후변경해야함!
